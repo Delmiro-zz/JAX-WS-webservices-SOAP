5cc5dce84171ea78588940b11fcfdf790ffd757c
Diego Delmiro <diegoalbuquerque03@gmail.com>
2019-02-17 16:32:34 -0300

    Entendendo os estilos Document e RPC do Soap.

--- src/br/com/caelum/estoque/ws/EstoqueWS.java
+++ src/br/com/caelum/estoque/ws/EstoqueWS.java
@@ -6,6 +6,10 @@ import javax.jws.WebMethod;
 import javax.jws.WebParam;
 import javax.jws.WebResult;
 import javax.jws.WebService;
+import javax.jws.soap.SOAPBinding;
+import javax.jws.soap.SOAPBinding.ParameterStyle;
+import javax.jws.soap.SOAPBinding.Style;
+import javax.jws.soap.SOAPBinding.Use;
 
 import br.com.caelum.estoque.modelo.item.Filtro;
 import br.com.caelum.estoque.modelo.item.Filtros;
@@ -17,6 +21,7 @@ import br.com.caelum.estoque.modelo.usuario.TokenDao;
 import br.com.caelum.estoque.modelo.usuario.TokenUsuario;
 
 @WebService
+@SOAPBinding(style = Style.DOCUMENT, use=Use.LITERAL, parameterStyle=ParameterStyle.BARE)
 public class EstoqueWS {
 
 	private ItemDao itemDao = new ItemDao();
@@ -29,7 +34,7 @@ public class EstoqueWS {
 		return new ListaItens(itens);
 	}
 
-	@WebMethod(operationName = "CadastrarItem")
+	@WebMethod(action="CadastrarItem", operationName = "CadastrarItem")
 	@WebResult(name = "item")
 	public Item cadastrarItem(@WebParam(name = "tokenUsuario", header = true) TokenUsuario tokenUsuario,
 			@WebParam(name = "item") Item item) throws AutorizacaoException {
