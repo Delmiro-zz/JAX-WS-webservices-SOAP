7609349ec202369afed8b43c435a9bd38ff23f36
Diego Delmiro <diegoalbuquerque03@gmail.com>
2019-02-17 12:18:26 -0300

    Adicionado filtro na chamada do serviço.

--- src/br/com/caelum/estoque/ws/EstoqueWS.java
+++ src/br/com/caelum/estoque/ws/EstoqueWS.java
@@ -1,11 +1,12 @@
 package br.com.caelum.estoque.ws;
 
 import java.util.List;
-
 import javax.jws.WebMethod;
+import javax.jws.WebParam;
 import javax.jws.WebResult;
 import javax.jws.WebService;
-
+import br.com.caelum.estoque.modelo.item.Filtro;
+import br.com.caelum.estoque.modelo.item.Filtros;
 import br.com.caelum.estoque.modelo.item.Item;
 import br.com.caelum.estoque.modelo.item.ItemDao;
 import br.com.caelum.estoque.modelo.item.ListaItens;
@@ -15,11 +16,12 @@ public class EstoqueWS {
 
 	private ItemDao itemDao = new ItemDao();
 	
-	@WebMethod(operationName="todosOsItens")
+	@WebMethod(operationName="TodosOsItens")
 	@WebResult(name="itens")
-	public ListaItens getItens() {
-		System.out.println("Chamando serviço getListaItens");
-		List<Item> itens = itemDao.todosItens();
+	public ListaItens getItens(@WebParam(name="filtros") Filtros filtros) {
+		System.out.println("Subindo o serviço...");
+		List<Filtro> listaFiltros = filtros.getLista();
+		List<Item> itens = itemDao.todosItens(listaFiltros);
 		return new ListaItens(itens);
 	}
 	
